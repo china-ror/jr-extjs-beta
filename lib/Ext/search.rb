9d0f3749ece77e27c644a5ca8ecb9f7d5346e515
Ye Jack <jack@jrails.org>
2012-05-29 04:53:24 -0700

    change search.rb

--- lib/Ext/search.rb
+++ lib/Ext/search.rb
@@ -1,73 +1,93 @@
 class Search
   
   LOGICALS = {
-               :eq      =>  '#{field} = ?',
-               :ne      =>  '#{field} <> ?',
-               :gt      =>  '#{field} > ?',
-               :lt      =>  '#{field} < ?',
-               :ge      =>  '#{field} >= ?',
-               :le      =>  '#{field} <= ?',
-               :any     =>  '#{field} LIKE ?',
-               :starts  =>  '#{field} LIKE ?',
-               :ends    =>  '#{field} LIKE ?',
-               :between =>  '#{field} BETWEEN ? AND ?',
-               :in      =>  '#{field} IN (?)',
-               :ni      =>  '#{field} NOT IN (?)'
+               :eq      =>  '#{column} = ?',
+               :ne      =>  '#{column} <> ?',
+               :gt      =>  '#{column} > ?',
+               :lt      =>  '#{column} < ?',
+               :ge      =>  '#{column} >= ?',
+               :le      =>  '#{column} <= ?',
+               :any     =>  '#{column} LIKE ?',
+               :starts  =>  '#{column} LIKE ?',
+               :ends    =>  '#{column} LIKE ?',
+               :between =>  '#{column} BETWEEN ? AND ?',
+               :in      =>  '#{column} IN (?)',
+               :ni      =>  '#{column} NOT IN (?)'
            }
 
   KEY_REGEX = /(eq|ne|gt|lt|ge|le|any|starts|ends|between|in|ni){1}_(\w+)_(for)_(\w+)/
 
-  attr_reader:tables,:fields,:search_params,:params,:conds,:init_search
-  attr_accessor:formats,:access,:not_access
+  attr_reader:tables,:fields,:params,:searchs
 
-  def initialize(search_params={})
-      @search_params = search_params || {}
-      @init_search = {}
+  def initialize(params={})
+      @searchs = HashWithIndifferentAccess.new
+      @params = params || HashWithIndifferentAccess.new
       @tables = []
       @fields = []
-      @params = []
-      @conds = ''
-      init_search_params
+      init_params
   end
 
   def copy(key,*to_keys)
       to_keys.each do |to_key|
-          @search_params[to_key] = @search_params[:key]
+          @params[to_key] = @params[key]
       end
-  end  
+  end 
 
-  def parse(operator='AND')
+  def formats(frees=nil)
       skip_keys = []
-      if @formats
-         skip_keys = @formats.scan(KEY_REGEX).collect { |skip| skip.join('_') }
-      else
-         @formats = ''
+      formats = ''
+      searchs_clone = @searchs.clone
+      if not frees.blank?
+         skip_keys = frees.scan(KEY_REGEX).collect { |skip| skip.join('_') }
+         searchs_clone.delete_if {|k,v| skip_keys.include?(k.to_s)}
+         formats << frees
       end
-      search_params_clone = @search_params.clone
-      search_params_clone.delete_if {|k,v| skip_keys.include?(k.to_s)}
-      @formats << " #{operator} " if not @formats.blank?
+      autos = searchs_clone.keys.join(' AND ')
+      if not autos.blank?
+         formats << ' AND ' if not frees.blank? 
+         formats << autos
+      end
+      if not formats.blank?
+         formats.insert(0,'(')
+         formats.insert(-1,')')
+      end
+      sqlparams = []
+      formats.gsub!(KEY_REGEX) do |m|
+          opt = @searchs[m]
+          oper = opt[:oper]
+          table = opt[:table]
+          field = opt[:field]
+          column = "#{ActiveRecord::Base.connection.quote_table_name(table)}.#{ActiveRecord::Base.connection.quote_column_name(field)}"
+          case oper
+          when :eq then
 
-      auto_formats << (search_params_clone.collect { |param| param[0] }).join(" #{operator} ")
-      keys = []
-      if @access or @not_access
-         keys = auto_formats.scan(KEY_REGEX).collect {|key| key.join('_')}
-      end
-      if @access
-         #raise "#{keys.inspect} not exist access keys!"
-      end
-      if @not_access
+          when :ne then
 
+          else
+
+          end
+          sqlparams << opt[:value]
+          eval('"' + LOGICALS[oper] + '"')
       end
-      @formats << auto_formats 
-      @formats.insert(0,'(')
-      @formats.insert(-1,')')
+      {:params=>sqlparams,:conds=>formats}
   end
 
   protected
-  def init_search_params
-      @search_params.each do |search_param|
-           @init_search[search_param[0]] = {:value=>search_param[1],:table=>'',:field=>'',:oper=>''}
+  def init_params
+      @params.each do |param|
+           kr = param[0].to_s.scan(KEY_REGEX).first
+           if kr
+              @tables << kr.last
+              @fields << "#{kr.last}.#{kr[1]}"
+              @searchs[param[0]] = {:value=>param[1],:table=>kr.last,:field=>kr[1],:oper=>kr.first.to_sym}
+           end
       end
+      @tables.uniq!
+      @fields.uniq!
+  end
+
+  def class_exist?(cls)
+       eval("defined?(#{cls}) && #{cls}.is_a?(Class)") == true
   end
 
 end
